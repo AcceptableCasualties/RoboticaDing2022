de9139e2f16a45919563bb80ac41af0de82fcea5
Niels van Dam <s1169063@student.windesheim.nl>
2022-03-14 16:00:32 +0100

    V2 ding

---
+++ LichtEnMotoren.ino
@@ -0,0 +1,70 @@
+//Arduino PWM Speed Control：
+#define ECHO 2
+#define TRIGGER 3
+#define E1 5
+#define M1 4
+#define E2 6
+#define M2 7
+// 9,10,11,12,13
+
+void setup() {
+   Serial.begin(115200); // Starting Serial Terminal
+   pinMode(ECHO, INPUT);
+   pinMode(TRIGGER, OUTPUT);
+   pinMode(8,OUTPUT);
+   pinMode(9,INPUT);
+   pinMode(10,INPUT);
+   pinMode(11,INPUT);
+   pinMode(12,INPUT);
+   pinMode(13,INPUT);
+   pinMode(M1, OUTPUT);
+   pinMode(M2, OUTPUT);
+}
+
+void loop() {
+   digitalWrite(8,HIGH);
+   long duration, cm;
+   digitalWrite(TRIGGER, LOW);
+   delayMicroseconds(2);
+   digitalWrite(TRIGGER, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(TRIGGER, LOW);
+
+   duration = pulseIn(ECHO, HIGH);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(cm);
+   Serial.print("cm\t");
+//   Serial.println();
+   for (int i = 9; i <= 13; i++) {
+    Serial.print(!digitalRead(i) ? "*" : "-");
+      if (!digitalRead(10)) {
+         digitalWrite(M1,HIGH);
+         digitalWrite(M2, HIGH);
+         analogWrite(E1, 60);   //PWM Speed Control
+         analogWrite(E2, 0);
+      } else if (!digitalRead(12)) {
+        digitalWrite(M1,HIGH);
+        digitalWrite(M2, HIGH);
+        analogWrite(E1, 0);   //PWM Speed Control
+        analogWrite(E2, 60);
+      } else if (!digitalRead(11)) {
+        digitalWrite(M1,HIGH);
+        digitalWrite(M2, HIGH);
+        analogWrite(E1, 60);   //PWM Speed Control
+        analogWrite(E2, 60);
+      } else {
+        digitalWrite(M1,HIGH);
+        digitalWrite(M2, HIGH);
+        analogWrite(E1, 0);   //PWM Speed Control
+        analogWrite(E2, 0);
+      }
+    }
+   Serial.println();
+//   delay(500);
+}
+
+
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
