fd5e9f9f3cde120a24c4a6f69a75ecefd266bf62
Niels van Dam <s1169063@student.windesheim.nl>
2022-03-29 15:00:34 +0200

    Changes to final countdown look.

--- ArduinoMazeSolver/ArduinoMazeSolver.ino
+++ ArduinoMazeSolver/ArduinoMazeSolver.ino
@@ -42,10 +42,12 @@ int log2 = 20;
 int log3 = 20;
 int log4 = 20;
 
+String path = "S";
 int totalIntersectionsCrossed = 0;
 bool isAtIntersection = false;
 String lastTurnDirection = "-";
 bool hasFinished = false;
+bool finishConcat = false;
 
 void setup() {
   //#ifdef DEBUG
@@ -77,6 +79,18 @@ start:
 
   if (hasFinished) {
     bcd.write("FI");
+    if (finishConcat == false) {
+      finishConcat = true;
+      path.replace("LU","T");
+      path.replace("LTL","D");
+      path.replace("SL","S");
+    }
+    Serial.println("F:::");
+    Serial.println(path);
+    countDownFrom10();
+    bcd.write("RE");
+    delay(2000);
+    rerun(path);
     return;
   } else {
     bcd.write(String(totalIntersectionsCrossed) + lastTurnDirection);
@@ -126,25 +140,25 @@ start:
   }
 
   if (LineSensors == "00100") {
-    motorDriver.setRightSpeed(70);
-    motorDriver.setLeftSpeed(70);
+    motorDriver.setRightSpeed(65);
+    motorDriver.setLeftSpeed(65);
     //    bcd.write("A1");/
     busy = true;
   } else if ( LineSensors == "01100" || LineSensors == "01000") {
-    motorDriver.setRightSpeed(70);
+    motorDriver.setRightSpeed(65);
     motorDriver.setLeftSpeed(0);
     //    bcd.write("C1");/
     busy = true;
   } else if ( LineSensors == "00110" || LineSensors == "00010") {
     motorDriver.setRightSpeed(0);
-    motorDriver.setLeftSpeed(70);
+    motorDriver.setLeftSpeed(65);
     //    bcd.write("C2");/
     busy = true;
   } else if ( LineSensors == "11111") {
 tsec:
     // Check forward.
-    motorDriver.setLeftSpeed(70);
-    motorDriver.setRightSpeed(70);
+    motorDriver.setLeftSpeed(65);
+    motorDriver.setRightSpeed(65);
     delTime = millis();
     //    bcd.write("Ch");/
     while (true) {
@@ -174,10 +188,11 @@ tsec:
     motorDriver.setRightSpeed(0);
     delay(700);
     turnLeft();
+    path = path + "U";
   } else if (LineSensors == "00111") {
     //    bcd.write("A2");/
-    motorDriver.setLeftSpeed(70);
-    motorDriver.setRightSpeed(70);
+    motorDriver.setLeftSpeed(65);
+    motorDriver.setRightSpeed(65);
     busy = true;
     delTime = millis();
     while (true) {
@@ -185,8 +200,8 @@ tsec:
       LineSensors = lineSensor.toString();
       curTime = millis();
       if (LineSensors == "00100" && curTime - delTime >= 250) {
-        motorDriver.setLeftSpeed(70);
-        motorDriver.setRightSpeed(70);
+        motorDriver.setLeftSpeed(65);
+        motorDriver.setRightSpeed(65);
         break;
       }
       if (LineSensors == "00000") {
@@ -214,24 +229,26 @@ void turnLeft() {
   lastTurnDirection = "L";
   delTime = millis();
   //  bcd.write("T1");/
-  motorDriver.setRightSpeed(70);
-  motorDriver.setLeftSpeed(-70);
+  motorDriver.setRightSpeed(65);
+  motorDriver.setLeftSpeed(-65);
   delay(250);
   while (true) {
-    motorDriver.setRightSpeed(70);
-    motorDriver.setLeftSpeed(-70);
+    motorDriver.setRightSpeed(65);
+    motorDriver.setLeftSpeed(-65);
     lineSensor.update();
     LineSensors = lineSensor.toString();
     curTime = millis();
     if (curTime - delTime >= 2000) {
       bcd.write("FI");
+      path = path + "F";
       hasFinished = true;
       motorDriver.setRightSpeed(0);
       motorDriver.setLeftSpeed(0);
-      return;//exit(0);
+      return;
     }
     if (LineSensors == "00100") {
       //      bcd.write("DO");/
+      path = path + "L";
       motorDriver.setRightSpeed(0);
       motorDriver.setLeftSpeed(0);
       delay(700);
@@ -256,6 +273,7 @@ void turnRight() {
     curTime = millis();
     if (curTime - delTime >= 2000) {
       bcd.write("FI");
+      path = path + "F";
       hasFinished = true;
       motorDriver.setRightSpeed(0);
       motorDriver.setLeftSpeed(0);
@@ -263,6 +281,7 @@ void turnRight() {
     }
     if (LineSensors == "00100") {
       //      bcd.write("DO");/
+      path = path + "R";
       motorDriver.setRightSpeed(0);
       motorDriver.setLeftSpeed(0);
       delay(700);
@@ -278,5 +297,65 @@ void simpleStop() {
   motorDriver.setLeftSpeed(0);
   delay(750);
   turnLeft();
+  return;
+}
+
+void countDownFrom10() {
+  bcd.write("10");
+  delay(1000);
+  for (int i=9; i>0; i--) {
+    bcd.write("0" + String(i));
+    delay(1000);
+    bcd.write("Fi");
+    delay(500);
+  }
+  bcd.write("00");
+  delay(500);
+  bcd.write("Fi");
+  delay(2000);
+}
+
+void rerun(String inPath) {
+  Serial.println("Debug");
+  int pathLength = inPath.length() + 1;
+  char path_array[pathLength];
+  inPath.toCharArray(path_array, pathLength);
+  for (char turn : path_array) {
+    if (turn == 'S') {
+      Serial.println("Start found.");
+      driveIdle();
+    } else if (turn == 'L') {
+      turnLeft();
+    } else if (turn == 'R') {
+      turnRight();
+    } else if (turn == 'F') {
+      motorDriver.setRightSpeed(0);
+      motorDriver.setLeftSpeed(0);
+      Serial.println("Finish!");
+    } else if (turn == 'D') {
+      driveIdle();
+    }
+  }
+}
+
+void driveIdle() {
+  motorDriver.setLeftSpeed(70);
+  motorDriver.setRightSpeed(70);
+  delay(250);
+  while (true) {
+    lineSensor.update();
+    LineSensors = lineSensor.toString();
+    if ( LineSensors == "01100" || LineSensors == "01000") {
+    motorDriver.setRightSpeed(70);
+    motorDriver.setLeftSpeed(0);
+    } else if ( LineSensors == "00110" || LineSensors == "00010") {
+    motorDriver.setRightSpeed(0);
+    motorDriver.setLeftSpeed(70);
+    } else if ( LineSensors == "11111" || LineSensors == "11100" || LineSensors == "00111") {
+      break;
+    }
+  }
+  motorDriver.setRightSpeed(0);
+  motorDriver.setLeftSpeed(0);
   return;
 }
